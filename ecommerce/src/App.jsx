bd9861d93647d9ac5bb62577d3a3288c27366ff6
ahmedam2317572-byte <ahmedam2317572@gmail.com>
2026-09-19 04:25:02 +0300

    Update the <a> to <link> and adding tracking page and orders page

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,5 +1,7 @@
 import { HomePage } from "./components/HomePage";
 import { CheckoutPage } from "./components/CheckoutPage";
+import { OrdersPage } from "./components/OrdersPage";
+import { TrackingPage } from "./components/TrackingPage";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/tracking" element={<TrackingPage />} />
       </Routes>
     </>
   );
