e0cc0fc41404ecdb0808e6b1f72b76f99595d26d
ahmedam2317572-byte <ahmedam2317572@gmail.com>
2026-09-21 02:29:32 +0300

    Lift cart up and share it between stages

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -3,14 +3,24 @@ import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/" element={<HomePage cart={cart} />} />
+        <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/tracking" element={<TrackingPage />} />
         <Route path="*" element={<h1>404 PAGE NOT FOUND</h1>} />
--- ecommerce/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce/src/pages/checkout/CheckoutPage.jsx
@@ -2,14 +2,14 @@ import { CheckoutHeader } from "./CheckoutHeader";
 import "./CheckoutPage.css";
 import "./CheckoutHeader.css";
 
-export function CheckoutPage() {
+export function CheckoutPage({ cart }) {
   return (
     <>
       <link rel="icon" href="/public/images/cart-favicon.png" />
 
       <title>Checkout</title>
 
-      <CheckoutHeader />
+      <CheckoutHeader cart={cart} />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
