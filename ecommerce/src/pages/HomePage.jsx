cf6d614b294fd8bdcc85190feaa3fd62e77027f0
ahmedam2317572-byte <ahmedam2317572@gmail.com>
2026-09-21 01:55:39 +0300

    Use axios to fetch the data

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products.js";
 import checkmarkIcon from "../assets/images/icons/checkmark.png";
 
 export function HomePage() {
-  fetch("http://localhost:3000/api/products").then((response) => {
-    response.json().then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
   });
 
   return (
