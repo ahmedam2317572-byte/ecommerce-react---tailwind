8143f4919974e387d48f86d4334b3a604e9f6568
ahmedam2317572-byte <ahmedam2317572@gmail.com>
2026-09-21 07:40:06 +0300

    Separate into smaller components

---
+++ ecommerce/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,42 @@
+import { Header } from "../../components/Header";
+import { OrderHeader } from "./OrderHeader";
+import { OrderDetails } from "./OrderDetails";
+import "./OrdersPage.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("api/orders?expand=products").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <link rel="icon" href="/public/images/orders-favicon.png" />
+
+      <title>Orders</title>
+
+      <Header cart={cart} />
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <div className="orders-grid">
+          {orders.map((order) => {
+            return (
+              <div key={order.id} className="order-container">
+                <OrderHeader order={order} />
+
+                <OrderDetails order={order} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
